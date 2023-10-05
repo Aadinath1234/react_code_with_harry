1899f33b0dc3ba81599cb777ba64c918ce822f52
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2023-10-05 13:28:23 +0530

    this code belongs to lecture #4 and #5

---
+++ #4 & #5/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name = "Wow bro"
+function App() {
+  return (
+    <>
+    <div className="blank">Lovely</div>
+
+    <nav>
+      <li>Home </li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+
+    <div className="container">
+      <h1>this is really {name}</h1>
+       to write the js we use curley brackets . 
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est eos, soluta corporis, consectetur aut earum aperiam quibusdam fugiat iusto laudantium ipsum cumque molestias?
+    </div>
+    </>
+  );
+}
+
+export default App;
